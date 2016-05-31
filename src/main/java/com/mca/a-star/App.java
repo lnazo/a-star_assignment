4ebf336b14396a52e40f7ae19264f3be4efa2f06
Lubabalo Nazo <lubabalo.nazo@multichoice.co.za>
2016-05-31 16:44:47 +0200

    initialised maven project using quickstart guide.

---
+++ src/main/java/com/mca/a-star/App.java
@@ -0,0 +1,13 @@
+package com.mca.a-star;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
